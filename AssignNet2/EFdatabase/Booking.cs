cfa9f32b460556e5332a815ac24b3e4557054a24
leonnamyers <Leonna.M.Myers@student.uts.edu.au>
2023-10-22 04:46:51 +1100

    +Setting up EntityFramework (booking)
    +Upgraded Winforms
    +Added Bookings and BookingContext
    
    trying to get it to work, then will be finished (then bug fixing, formatting etc)

---
+++ AssignNet2/EFdatabase/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.com
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public string Email { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int CompanyType { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
---
+++ AssignNet2/EFdatabase/BookingContext.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.com
+{
+    public class BookingContext : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    => optionsBuilder.UseSqlite("Data Source=bookings_details.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Booking>().HasData(
+                new Booking
+                {
+                    BookingId = 1,
+                    Email = "test@sample.com",
+                    CheckIn = new DateTime(2023, 10, 1, 14, 30, 0),
+                    CheckOut = new DateTime(2023, 10, 5, 10, 00, 0),
+                    CompanyType = 1,
+                    CompanyId = 1,
+                }
+                );
+        }
+    }
+}
